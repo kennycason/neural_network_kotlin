d4c4bee95b4bc10b844812b3999da0db19d2acd1
Kenny Cason <kenneth.cason@gmail.com>
2018-01-20 20:52:34 -0800

    add image output of learned mnist data

--- src/test/kotlin/com/kennycason/nn/MNISTAutoEncoderTest.kt
+++ src/test/kotlin/com/kennycason/nn/MNISTAutoEncoderTest.kt
@@ -2,6 +2,7 @@ package com.kennycason.nn
 
 import com.kennycason.nn.data.*
 import com.kennycason.nn.math.Errors
+import org.jblas.DoubleMatrix
 import org.junit.Test
 import java.util.*
 
@@ -14,14 +15,14 @@ class MNISTAutoEncoderTest {
         val visibleSize = 28 * 28
         val hiddenSize = (visibleSize * 0.75).toInt()
         val layer = AutoEncoder(
-                learningRate = 0.05,
+                learningRate = 0.1,
                 visibleSize = visibleSize,
                 hiddenSize = hiddenSize,
                 log = false)
 
         val start = System.currentTimeMillis()
         val rand = Random()
-        (0.. 100000).forEach { i ->
+        (0.. 100_000).forEach { i ->
             val x = xs.getRow(rand.nextInt(xs.rows))
             layer.learn(x, 1)
 
@@ -33,19 +34,33 @@ class MNISTAutoEncoderTest {
         }
         println("${System.currentTimeMillis() - start}ms")
 
+        val generatedOutputs = DoubleMatrix(xs.rows, xs.columns)
+
+        // calculate error
         var errorSum = 0.0
         (0 until xs.rows).forEach{ i ->
             val x = xs.getRow(i)
-            val error = Errors.compute(x, layer.feedForward(x))
+            val y = layer.feedForward(x)
+            val error = Errors.compute(x, y)
+            generatedOutputs.putRow(i, y)
 
             if (i % 1000 == 0) {
                 println("input:\n" + PrintUtils.toPixelBox(x.toArray(), 28, 0.5))
-                println("output:\n" + PrintUtils.toPixelBox(layer.feedForward(x).toArray(), 28, 0.7))
+                println("output:\n" + PrintUtils.toPixelBox(y.toArray(), 28, 0.7))
                 println("error: $error")
             }
             errorSum += error
         }
         println("total error: " + (errorSum / (xs.rows)))
+
+        // write to composite image
+        val image = CompositeImageWriter.write(
+                data = generatedOutputs,
+                matrixImageDecoder = MatrixGrayScaleImageDecoder(rows = 28),
+                rows = 245,
+                cols = 245)
+
+        image.save("/tmp/mnist_autoencoder_generated.png")
     }
 
 }
